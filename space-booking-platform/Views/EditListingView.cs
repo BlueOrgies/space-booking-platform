5455f72df74a1ff94be722a4645d6f6a50867625
kristianeo <olsen.kristiane@gmail.com>
2026-04-07 13:05:34 +0200

    Feat: Added method to edit value in listing

---
+++ space-booking-platform/Views/EditListingView.cs
@@ -0,0 +1,71 @@
+using space_booking_platform.Models;
+using space_booking_platform.Services;
+using Spectre.Console;
+
+namespace space_booking_platform.Views;
+
+public class EditListingView
+{
+    //TODO: Create a method to choose listings from a list and return listingId
+    public static void EditListing(int listingId)
+    {
+        var prompt = new SelectionPrompt<string>()
+            .Title("[bold]What would you like to edit?:[/]")
+            .WrapAround()
+            .AddChoices("Title", "Description", "Transportation Method", "Origin", "Destination",
+                "Date", "Duration", "Capacity", "Price");
+        string edit = AnsiConsole.Prompt(prompt);
+
+        switch (edit)
+        {
+            case "Title":
+                string title = AnsiConsole.Ask<string>("New title: ");
+                ListingService.EditListingInDb("title", title, listingId);
+                break;
+            case "Description":
+                string description = AnsiConsole.Ask<string>("New description: ");
+                ListingService.EditListingInDb("description", description, listingId);
+                break;
+            case "Transportation method":
+                string transportation = AnsiConsole.Ask<string>("Transportation method: ");
+                ListingService.EditListingInDb("transportationMethod", transportation, listingId);
+                break;
+            case "Origin":
+                string origin = AnsiConsole.Ask<string>("Edit origin: ");
+                ListingService.EditListingInDb("origin", origin, listingId);
+                break;
+            case "Destination":
+                string destination = AnsiConsole.Ask<string>("Edit destination: ");
+                ListingService.EditListingInDb("destination", destination, listingId);
+                break;
+            case "Date":
+                DateTime date = AnsiConsole.Ask<DateTime>("New date and time (yyyy-MM-dd HH:mm): ");
+                ListingService.EditListingInDb("date", date.ToString(), listingId);
+                break;
+            case "Duration":
+                int duration = AnsiConsole.Ask<int>("Edit duration: ");
+                string durationType = AnsiConsole.Ask<string>("Edit duration type: ");
+                ListingService.EditListingInDb("duration", duration.ToString(), listingId);
+                ListingService.EditListingInDb("durationType", durationType, listingId);
+                break;
+            case "Capacity":
+                prompt = new SelectionPrompt<string>()
+                    .Title("[bold]Capacity unit:[/]")
+                    .AddChoices(Enum.GetNames<ListingCapacityUnit>());
+                var capacityUnit = AnsiConsole.Prompt(prompt);
+                int capacity = AnsiConsole.Ask<int>($"Edit duration ({capacityUnit}): ");
+                ListingService.EditListingInDb("capacity", capacity.ToString(), listingId);
+                ListingService.EditListingInDb("capacityUnit", capacityUnit, listingId);
+                break;
+            case "Price":
+                prompt = new SelectionPrompt<string>()
+                    .Title("[bold]Price unit:[/]")
+                    .AddChoices(Enum.GetNames<ListingPriceUnit>());
+                var priceUnit = AnsiConsole.Prompt(prompt);
+                int price = AnsiConsole.Ask<int>($"Edit duration ({priceUnit}): ");
+                ListingService.EditListingInDb("capacity", price.ToString(), listingId);
+                ListingService.EditListingInDb("capacityUnit", priceUnit, listingId);
+                break;
+        }
+    }
+}
