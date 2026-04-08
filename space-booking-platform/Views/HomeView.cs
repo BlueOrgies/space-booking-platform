5b226cef3f574e16a8a5f147271b301f8fb72c2e
Aleksander Aasen <alekssaasen@gmail.com>
2026-04-08 12:40:42 +0200

    chore: 'logout' exits program, instead of throwing exception.

--- space-booking-platform/Views/HomeView.cs
+++ space-booking-platform/Views/HomeView.cs
@@ -21,8 +21,7 @@ public class HomeView(AppState state)
         {
             "Log in"             => "Login",
             "Register"          => "Register",
-            "Log out"            => "Logout",
-            _                   => null 
+            "Log out"            => null
         };
     }
 }
