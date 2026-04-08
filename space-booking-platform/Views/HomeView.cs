15c9aad7d8fb97bee907cc309e9d9e7f1b544ead
Aleksander Aasen <alekssaasen@gmail.com>
2026-04-08 11:54:22 +0200

    feat: Added HomeView and Display method, that will be used in viewhandler

--- space-booking-platform/Views/HomeView.cs
+++ space-booking-platform/Views/HomeView.cs
@@ -1,6 +1,28 @@
-﻿namespace space_booking_platform.Views;
+﻿using Spectre.Console;
 
-public class HomeView
+namespace space_booking_platform.Views;
+
+public class HomeView(AppState state)
 {
-    
+    public string? Display()
+    {
+        AnsiConsole.Clear();
+        AnsiConsole.MarkupLine("[green]✓ Build completed successfully[/]");
+
+        var choices = new List<string> { "Log in", "Register", "Log out"};
+
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Where do you want to go?")
+                .HighlightStyle(new Style(Color.Yellow))
+                .AddChoices(choices));
+        
+        return choice switch
+        {
+            "Log in"             => "Login",
+            "Register"          => "Register",
+            "Log out"            => "Logout",
+            _                   => null 
+        };
+    }
 }
