b45f1679dd368a81ef86877759505537d25810b3
Aleksander Aasen <alekssaasen@gmail.com>
2026-04-16 15:26:05 +0200

    feat: add basic myBookingsView with database connection

--- space-booking-platform/Views/MyBookingsView.cs
+++ space-booking-platform/Views/MyBookingsView.cs
@@ -1,6 +1,22 @@
 ﻿namespace space_booking_platform.Views;
 
-public class MyBookingsView
+public class MyBookingsView(AppState state)
 {
-    
+    public string? Display()
+    {
+        BookingService bookingService = new BookingService();
+        List<Booking?> allBookings = bookingService.GetBookings(state.currentUUID);
+
+        while (true)
+        {
+            AnsiConsole.Clear();
+            AnsiConsole.MarkupLine("[bold green]My Bookings[/]");
+            if (allBookings.Count == 0)
+            {
+                AnsiConsole.MarkupLine("You have no bookings.");
+                AnsiConsole.Prompt(new SelectionPrompt<string>().AddChoices("Go back to profile"));
+                return "ProfileView";
+            }
+        }
+    }
 }
